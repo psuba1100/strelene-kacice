f1fb84539e2cce0b0f902b1337dcc0c838e587e6
psuba1100 <92215854+psuba1100@users.noreply.github.com>
2024-02-29 20:37:18 +0100

    modified:   pages/boom.php 	modified:   pages/game.php

--- pages/boom.php
+++ pages/boom.php
@@ -28,6 +28,40 @@ $e->getMessage();
     header('Location: ../index.php');
     exit();
 //}
+
+
+
+/*if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+// Assuming IDHraca is 1 for now
+$IDHraca = 1;
+$karty = implode(',', $data['dataToSend']); // Convert array to comma-separated string
+
+try {
+    $stmt = $pdo->prepare("INSERT INTO `kartyhracov` (`IDHraca`, `Karty`) VALUES (:IDHraca, :Karty)");
+    $stmt->bindParam(':IDHraca', $IDHraca);
+    $stmt->bindParam(':Karty', $karty);
+    $stmt->execute();
+    echo json_encode(array("status" => "success"));
+} catch (PDOException $e) {
+    echo json_encode(array("status" => "error", "message" => $e->getMessage()));
+}
+exit();
+}*/
+
+    /*$ID_hraca = 0;
+    $sql = "INSERT INTO `kartyhracov` (`IDHraca`) VALUES (:IDHraca)";
+    try {
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(":IDHraca", $ID_hraca, PDO::PARAM_STR);
+        $stmt->execute();
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }*/
+
 ?>
 
 <button class="btn btn-custom-primary btn-lg back" id="fullScreen">fs</button>
